eca8614f1f14ab7ff50500b999fd1f3dcc35c9fb
charlesAcmen <15591542871@163.com>
2025-10-28 11:55:36 +0800

    2nd class

--- include/kvstore/kvserver.h
+++ include/kvstore/kvserver.h
@@ -11,14 +11,16 @@ class KVServer {
 
 public:
     KVServer(int me,std::shared_ptr<raft::Raft> raft,int maxRaftState);
-    // Public API - exported as RPC handlers
+
+    std::shared_ptr<KVServer> StartKVServer()
+
     // Called by network RPC from Clerk
     void Put(const std::string& key, const std::string& value);
     void Append(const std::string& key, const std::string& arg);
     void Get(const std::string& key);
 private:
     int me_;
-    std::atomic<bool> dead_{false};
+    std::atomic<int32_t> dead_{0};  // set by Kill()    
     std::shared_ptr<raft::Raft> rf;
     std::shared_ptr<KVStateMachine> kvStateMachine_;
     int maxRaftState_{-1}; //-1 means no snapshotting
